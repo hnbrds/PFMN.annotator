62cb8da872b9e34530d7ca241365e3b800f3236e
성진영 <jysung710@gmail.com>
2017-03-31 23:36:51 +0900

    Bug fix / Frame count

--- src/app/video-component/video-component.component.ts
+++ src/app/video-component/video-component.component.ts
@@ -1,51 +1,98 @@
-import { Component, ElementRef, EventEmitter, OnInit, AfterViewInit, ViewChild} from '@angular/core';
-import { VideoJSPlayer, videojs } from 'video.js'
+import { Component, ElementRef, Renderer2, EventEmitter, OnInit, AfterViewInit, ViewChild} from '@angular/core';
+import { Observable } from 'rxjs/Rx'
+import * as videojs from 'video.js'
 
 @Component({
   selector: 'video-component',
   templateUrl: './video-component.component.html',
   styleUrls: ['./video-component.component.css']
 })
-export class VideoComponentComponent implements OnInit {
+export class VideoComponentComponent implements OnInit, AfterViewInit{
+  // video property
   @ViewChild('vid') video : ElementRef
-  private _elementRef: ElementRef
-  private videoJSplayer : VideoJSPlayer
+  private videoWidth : number = 0
+  private videoHeight : number = 0
+  // Mouse coords
+  private highLights : any[] = []
+  private coordSequence : any[] = []
+  private pixelX : number = 0
+  private pixelY : number = 0
+  // Frame counter
+  private framecount : number = 0
+  private fps : number = 30
 
-  private highLights : any
-  private pixelX : any
-  private pixelY : any
+  // canvas property
+  //@ViewChild("draw") canvas : ElementRef
+  private canvas : any
+  private context : any
 
-  constructor(elementRef: ElementRef) {
-        this._elementRef = elementRef
+
+  constructor(private elementRef: ElementRef, private rd : Renderer2) {
   }
+
   ngOnInit() {
-    this.pixelX = 0;
-    this.pixelY = 0;
+    let timer = Observable.timer(0,20);
+    timer.subscribe(t=> {
+      this.updateFrameCount(t);
+    });
   }
 
-  getElementCSSSize(element) {
-    var cs = getComputedStyle(element);
-    var w = parseInt(cs.getPropertyValue("width"), 10);
-    var h = parseInt(cs.getPropertyValue("height"), 10);
-    return {width: w, height: h}
+  ngAfterViewInit() {
+    this.canvasInit();
   }
 
-  onMouseClick(event : MouseEvent) {
-    //console.log(this.video);
-    let size = this.getElementCSSSize(event.target);
+  canvasInit() {
+    this.canvas = document.getElementById('draw');
+    this.context = this.canvas.getContext('2d');
+    console.log(document.getElementById('draw'));
+    console.log(this.context);
+    /*
+    this.video.nativeElement.addEventListener('play', function() {
+      if(this.video.nativeElement.paused || this.video.nativeElement.ended)
+        return false;
+      this.canvas.drawImage(3, 0, 0, 5, 1);
+    }, false);*/
+  }
+
+  getVideoProperty() {
+    this.videoWidth = this.video.nativeElement.videoWidth;
+    this.videoHeight = this.video.nativeElement.videoHeight;
+  }
+
+  updateFrameCount(t : number) {
+    var tmp = Math.floor(this.video.nativeElement.currentTime * this.fps);
+    if(this.framecount != tmp){
+      this.framecount = tmp;
+      this.coordSequence.push([tmp, this.pixelX, this.pixelY]);
+    }
+  }
+
+  cutOff(x, y, width, height) {
+    x = Math.max(Math.min(x, width-1), 0);
+    y = Math.max(Math.min(y, height-1), 0);
+    return {posX: x, posY:y}
+  }
+
+  onMouse(event : MouseEvent) {
+    console.log();
     var videodat = this.video.nativeElement;
-
     var videoWidth = videodat.videoWidth;
     var videoHeight = videodat.videoHeight;
-    var scaleX = videoWidth / size.width;
-    var scaleY = videoHeight / size.height;
+    var clientWidth = videodat.clientWidth;
+    var clientHeight = videodat.clientHeight;
+    var scaleX = videoWidth / clientWidth;
+    var scaleY = videoHeight / clientHeight;
 
-    var offsetLeft = videodat.offsetLeft + videodat.offsetParent.offsetLeft;
-    var offsetTop = videodat.offsetTop;
-    var x = ((event.clientX - offsetLeft)*scaleX) + 0.5 | 0; // round to integer
-    var y = ((event.clientY - offsetTop)*scaleY) + 0.5 | 0;
-
-    this.pixelX = x;
-    this.pixelY = y;
+    let pos = this.cutOff(event.offsetX, event.offsetY, clientWidth, clientHeight);
+    this.pixelX = (pos.posX * scaleX) + 0.5 | 0;
+    this.pixelY = (pos.posY * scaleY) + 0.5 | 0;
+    if(event.type == "mousemove"){
+    }
+    if(event.type == "click") {
+      console.log(event);
+      console.log(this.video);
+      this.highLights.push([this.pixelX, this.pixelY]);
+      this.context.rect(event.offsetX-10, event.offsetX-10, 20, 20);
+    }
   }
 }
