5d3d008a9e172d0f4e80599643b01711679d3971
성진영 <jysung710@gmail.com>
2017-04-02 15:48:27 +0900

    New line bug fix, serve-index

--- server/routes/log.js
+++ server/routes/log.js
@@ -18,7 +18,7 @@ function writeLog(data, callback) {
   console.log('Saving data to ' + __dirname+'/../../log/log.json');
   console.log(data.vid);
   //fs.writeFile(path.join(__dirname+'/../../../log/'+data.vid+'.json'), JSON.stringify(data), callback);
-  fs.appendFile(path.join(__dirname+'/../../../log/'+data.vid+'.json'), JSON.stringify(data), callback);
+  fs.appendFile(path.join(__dirname+'/../../../log/'+data.vid+'.json'), JSON.stringify(data)+'\n', callback);
 }
 
 module.exports = router;
